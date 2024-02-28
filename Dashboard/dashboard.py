7de4ad2d599ea2b2cd8190f7fdd94c64adecc153
nurqoneah <nurulqoniah313@gmail.com>
2024-02-28 21:26:53 +0700

    benerin path

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -113,13 +113,12 @@ class Map:
         self.st.pyplot()
         return ax 
     
-    
 
 
-geolocation_df = pd.read_csv('./Dashboard/geolocation_dataset.csv')
-# geolocation = geolocation_df.drop_duplicates(subset='customer_unique_id')
 
-all_df = pd.read_csv("./Dashboard/data_df.csv")
+geolocation_df = pd.read_csv('./geolocation_dataset.csv')
+
+all_df = pd.read_csv("./data_df.csv")
 datetime_columns = ["shipping_limit_date", "review_creation_date","review_answer_timestamp","order_purchase_timestamp","order_approved_at","order_delivered_carrier_date","order_delivered_customer_date","order_estimated_delivery_date"]
 all_df.sort_values(by="order_approved_at", inplace=True)
 all_df.reset_index(inplace=True)
